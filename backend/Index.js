28bd1e5b92b114b3ca5f16a04a50d49ca00b9fa3
gopalksahoo063 <gopal@havahavai.com>
2024-11-17 02:49:46 +0530

    added proper urls

--- backend/Index.js
+++ backend/Index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://cms-matrix.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
--- centrailized_dashboard/src/utils/axios.js
+++ centrailized_dashboard/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001',
+    baseURL: 'https://cmsmatrix.onrender.com',
   timeout: 30000,
   withCredentials: true,
   headers: {
